a2688ab83d4be6e7232ea7581608c37727e64179
CK-Check <chetandell3000@gmail.com>
2026-05-03 22:15:58 -0400

    Added the title functionality with axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,21 +73,21 @@ public_users.post("/register", (req,res) => {
 // });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const titleName = req.params.title;
+// public_users.get('/title/:title',function (req, res) {
+//     const titleName = req.params.title;
   
-    const allBooks = Object.values(books);
+//     const allBooks = Object.values(books);
   
-    const filteredBooks = allBooks.filter(
-      (book) => book.title.toLowerCase() === titleName.toLowerCase()
-    );
+//     const filteredBooks = allBooks.filter(
+//       (book) => book.title.toLowerCase() === titleName.toLowerCase()
+//     );
   
-    if (filteredBooks.length > 0) {
-      return res.status(200).send(JSON.stringify(filteredBooks, null, 4));
-    } else {
-      return res.status(404).json({ message: "No books found with this title" });
-    }
-});
+//     if (filteredBooks.length > 0) {
+//       return res.status(200).send(JSON.stringify(filteredBooks, null, 4));
+//     } else {
+//       return res.status(404).json({ message: "No books found with this title" });
+//     }
+// });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
@@ -166,6 +166,35 @@ public_users.get('/', async function (req, res) {
     } catch (error) {
       return res.status(500).json({
         message: "Error fetching books by author",
+        error: error.message
+      });
+    }
+  });
+
+  public_users.get('/title/:title', async function (req, res) {
+    const titleName = req.params.title;
+  
+    try {
+      const response = await axios.get('http://localhost:5000/customer');
+  
+      const booksData = response.data;
+      const allBooks = Object.values(booksData);
+  
+      const filteredBooks = allBooks.filter(
+        (book) => book.title.toLowerCase() === titleName.toLowerCase()
+      );
+  
+      if (filteredBooks.length > 0) {
+        return res.status(200).send(JSON.stringify(filteredBooks, null, 4));
+      } else {
+        return res.status(404).json({
+          message: "No books found with this title"
+        });
+      }
+  
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error fetching books by title",
         error: error.message
       });
     }
