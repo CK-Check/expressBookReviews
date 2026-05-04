921b9f50edd42e0f10df526c67f78133c4b96da8
CK-Check <chetandell3000@gmail.com>
2026-05-03 22:11:43 -0400

    Added the axios isbn callback

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,22 +38,22 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
-});
+// public_users.get('/',function (req, res) {
+//   return res.status(200).send(JSON.stringify(books, null, 4));
+// });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
+// public_users.get('/isbn/:isbn',function (req, res) {
+//     const isbn = req.params.isbn;
 
-    const book = books[isbn];
+//     const book = books[isbn];
   
-    if (book) {
-      return res.status(200).send(JSON.stringify(book, null, 4));
-    } else {
-      return res.status(404).json({ message: "Book not found" });
-    }
-});
+//     if (book) {
+//       return res.status(200).send(JSON.stringify(book, null, 4));
+//     } else {
+//       return res.status(404).json({ message: "Book not found" });
+//     }
+// });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -113,6 +113,30 @@ public_users.get('/', async function (req, res) {
     } catch (error) {
       return res.status(500).json({
         message: "Error fetching books",
+        error: error.message
+      });
+    }
+  });
+
+  public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+  
+    try {
+      const response = await axios.get('http://localhost:5000/customer');
+  
+      const booksData = response.data;
+  
+      const book = booksData[isbn];
+  
+      if (book) {
+        return res.status(200).send(JSON.stringify(book, null, 4));
+      } else {
+        return res.status(404).json({ message: "Book not found" });
+      }
+  
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error fetching book details",
         error: error.message
       });
     }
