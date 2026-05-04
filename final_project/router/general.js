b35d404abaf82fffd6e3ddbbff8ed6193b0a8843
CK-Check <chetandell3000@gmail.com>
2026-05-03 21:35:19 -0400

    Added title and author functionalities

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,14 +30,36 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const authorName = req.params.author;
+  
+    const allBooks = Object.values(books);
+  
+    const filteredBooks = allBooks.filter(
+      (book) => book.author.toLowerCase() === authorName.toLowerCase()
+    );
+  
+    if (filteredBooks.length > 0) {
+      return res.status(200).send(JSON.stringify(filteredBooks, null, 4));
+    } else {
+      return res.status(404).json({ message: "No books found for this author" });
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const titleName = req.params.title;
+  
+    const allBooks = Object.values(books);
+  
+    const filteredBooks = allBooks.filter(
+      (book) => book.title.toLowerCase() === titleName.toLowerCase()
+    );
+  
+    if (filteredBooks.length > 0) {
+      return res.status(200).send(JSON.stringify(filteredBooks, null, 4));
+    } else {
+      return res.status(404).json({ message: "No books found with this title" });
+    }
 });
 
 //  Get book review
